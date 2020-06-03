4c25f7ce66b4a056c99c90b3efd6453d8709a816
Ahmet Ince <ahmetince771@gmail.com>
2020-06-03 23:01:18 +0100

    added dark theme

--- src/app/nav/menu-button/menu-button.component.ts
+++ src/app/nav/menu-button/menu-button.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-menu-button',
-  templateUrl: './menu-button.component.html',
-  styleUrls: ['./menu-button.component.scss'],
+  selector: "app-menu-button",
+  templateUrl: "./menu-button.component.html",
+  styleUrls: ["./menu-button.component.scss"],
 })
 export class MenuButtonComponent implements OnInit {
   @Input() title?;
+  darkMode = false;
 
-  constructor() { }
+  constructor() {
+    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)");
+    prefersDark.addListener(e => {
+      this.setAppTheme(e.matches);
+    })
+  }
 
   ngOnInit() {}
 
+  toggleTheme() {
+    this.darkMode = !this.darkMode;
+    this.setAppTheme(this.darkMode);
+  }
+
+  setAppTheme(dark) {
+    this.darkMode = dark;
+
+    if (this.darkMode) {
+      document.body.classList.add("dark");
+    }
+    else {
+      document.body.classList.remove("dark");
+    }
+  }
 }
