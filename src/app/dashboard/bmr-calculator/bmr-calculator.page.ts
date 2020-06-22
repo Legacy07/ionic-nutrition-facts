2e4bc058f1ef40d3ae1dcba8952caf3e0647bdbb
Ahmet Ince <ahmetince771@gmail.com>
2020-06-22 23:46:50 +0100

    bmr calculator view added and calculation mostly done

--- src/app/dashboard/bmr-calculator/bmr-calculator.page.ts
+++ src/app/dashboard/bmr-calculator/bmr-calculator.page.ts
@@ -8,10 +8,33 @@ import { Component, OnInit } from '@angular/core';
 export class BmrCalculatorPage implements OnInit {
 
   public title = "BMR Calculator";
+  public isKilogram: boolean = true;
+  public isCentimetres: boolean = true;
+  public calculatedResult: number;
+
+  public ageInput: number;
+  public heightInput: number;
+  public weightInput: number;
+  public selectedSex: string;
+  public selectedDesire: string;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggleWeight() {
+    this.isKilogram = !this.isKilogram;
+  }
+
+  toggleHeight() {
+    this.isCentimetres = !this.isCentimetres;
+  }
+
+  calculate() {
+    // for kg and ct 
+    if (this.heightInput && this.weightInput && this.ageInput) {
+      this.calculatedResult = 66.47 + (13.47 * this.weightInput) + (5 * this.heightInput) - (6.8 * this.ageInput);
+    }
+  }
 }
